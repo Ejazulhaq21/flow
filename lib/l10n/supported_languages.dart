746b50da98982c744a68541d85fe2fba690b3ef6
Ejazulhaq Malik ( AJ ) <51711171+Ejazulhaq21@users.noreply.github.com>
2025-12-11 17:56:05 +0430

    Update supported_languages.dart

--- lib/l10n/supported_languages.dart
+++ lib/l10n/supported_languages.dart
@@ -16,5 +16,6 @@ final Map<Locale, (String, String)> supportedLanguages = {
   const Locale("es", "ES"): ("Spanish (Spain)", "Español (España)"),
   const Locale("uk", "UA"): ("Ukrainian (Ukraine)", "Українська (Україна)"),
   const Locale("ar"): ("Arabic", "العربية"),
-    const Locale("ps", "AF"):  ("Pashto (Afghanistan)", ("Pashto", "پښتو"),
+  const Locale("ps"): ("Pashto", "پښتو"),
+
 };
